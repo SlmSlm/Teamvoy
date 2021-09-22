44c5b7009c832edc16eb8ba4d4a3ac144925f9ac
mac <mac@macs-MacBook-Pro.local>
2021-09-22 22:56:35 +0300

    api connected

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import * as axios from "axios";
 
 function App() {
+  function loadMore() {
+   getPokemons();
+  }
+  const [pokemon, setPokemon] = useState([]);
+  let [currentLimit, setLimit] = useState(12);
+
+  const getPokemons = async () => {
+    return await axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=${currentLimit}`)
+      .then((res) => {
+        setPokemon(res.data.results.map((p) => p.name));
+        setLimit(currentLimit += 12);
+      });
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PokemonList pokemon={pokemon} />
+      <button onClick={loadMore}>Load more</button>
     </div>
   );
 }
