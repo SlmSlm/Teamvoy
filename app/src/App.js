d3f7f71e5da585e3321949874d33228254485bd2
mac <mac@macs-MacBook-Pro.local>
2021-09-23 21:45:35 +0300

    Get Pokemons data

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,34 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import PokemonList from "./PokemonList";
 import * as axios from "axios";
 
 function App() {
   function loadMore() {
-   getPokemons();
+    getPokemons();
   }
-  const [pokemon, setPokemon] = useState([]);
+
   let [currentLimit, setLimit] = useState(12);
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonsId, setId] = useState([]);
 
   const getPokemons = async () => {
-    return await axios
+    let array = [];
+    await axios
       .get(`https://pokeapi.co/api/v2/pokemon?limit=${currentLimit}`)
       .then((res) => {
-        setPokemon(res.data.results.map((p) => p.name));
-        setLimit(currentLimit += 12);
+        res.data.results.map((p) => {
+          return axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${p.name}`)
+            .then((resp) => {
+              return (
+                array.push(resp.data), setPokemons(array), setId(resp.data.id)
+                // console.log(pokemonsId)
+              );
+            });
+        });
+        setLimit((currentLimit += 12));
       });
+    // console.log(pokemons)
   };
 
   useEffect(() => {
@@ -25,7 +37,21 @@ function App() {
 
   return (
     <div>
-      <PokemonList pokemon={pokemon} />
+      {pokemons.map((p) => {
+        // console.log(pokemons);
+        // console.log(p);
+        return (
+          <div key={p.id}>
+            <div>
+              {p.name}
+              <img src={p.sprites.front_default} alt="Pokemon" />
+              {p.types.map((types) => (
+                <div> {types.type.name} </div>
+              ))}
+            </div>
+          </div>
+        );
+      })}
       <button onClick={loadMore}>Load more</button>
     </div>
   );
